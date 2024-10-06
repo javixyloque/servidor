7ace203b3b7c303d45d570faa7c9eb5be65b991c
javixyloque <perosa1000@gmail.com>
2024-10-06 15:45:52 +0200

    Commit casa ults ejercicios

--- 221mi_altura.php
+++ 221mi_altura.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="Javi" content="Javier Alvarez Centeno">
-    <title>220 - Alturas</title>
+    <title>221 - Mi Altura</title>
 </head>
 <body>
-    <table border="solid 2px">
+<table border="solid 2px">
         <thead><th colspan="2">Alturas alumnos</th></thead>
     <?php
         $altura = [
@@ -18,14 +18,30 @@
             "Perico" => 162
         ];
 
+        $altMedia = array_sum($altura)/count($altura);
+
         foreach ($altura as $key => $value) {
             echo "<tr>";
             echo "<td>$key</td><td>$value cm</td>";
             echo "</tr>";
         }
-        echo "<tr><td>Media</td><td>".array_sum($altura)/count($altura)." cm</td></tr>";
+        echo "<tr><td>Media</td><td><strong>$altMedia cm</strong></td></tr>";
 
     ?>
     </table>
+
+    <?php
+        $miAltura = $_GET['alt'];
+        if (isset($miAltura)) {
+            echo "<br><br><h3>Tu altura : $miAltura cm</h3><br><br>";
+            if ($miAltura < $altMedia) {
+                echo "Tu altura esta por debajo de la media &#x2639;";
+            } else {
+                echo "Tu altura esta por encima de la media !!  &#x263a;";
+            }
+        }
+        
+        
+    ?>
 </body>
 </html>
