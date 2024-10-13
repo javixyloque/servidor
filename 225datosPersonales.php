4dc913fa1d8759256248ed6b8bd7862b31f368dc
javixyloque <perosa1000@gmail.com>
2024-10-13 14:48:14 +0200

    13 OCTUBRE

--- 225datosPersonales.php
+++ 225datosPersonales.php
@@ -7,25 +7,25 @@
     <title>225 - Datos Personales</title>
 </head>
 <body>
-    <table>
-        <thead>
+    <table border="solid 2px" cellspacing="0" height="300px" width="1000px">
+        <thead bgcolor="605678">
             <th>Nombre</th>
             <th>Apellidos</th>
             <th>Correo electronico</th>
             <th>Año de nacimiento</th>
             <th>Telefono</th>
         </thead>
-        <tbody>
+        <tbody align="center" bgcolor="8ABFA3">
+            <!--- AQUI NO ABRO NI LA SESION DIRECTAMENTE PORQUE NO ME HACE FALTA TRABAJAR CON DATOS ANTERIORES -->
             <?php
-            session_start();
                 $nombre = isset($_GET['nombre']) ? $_GET['nombre'] : '';
-                $nombre = isset($_GET['prApe']) ? $_GET['prApe'] : '';
-                $nombre = isset($_GET['sgApe']) ? $_GET['sgApe'] : '';
-                $nombre = isset($_GET['email']) ? $_GET['email'] : '';
-                $nombre = isset($_GET['anioNac']) ? $_GET['anioNac'] : '';
-                $nombre = isset($_GET['telefono']) ? $_GET['telefono'] : '';
+                $prApe = isset($_GET['prApe']) ? $_GET['prApe'] : '';
+                $sgApe = isset($_GET['sgApe']) ? $_GET['sgApe'] : '';
+                $email = isset($_GET['email']) ? $_GET['email'] : '';
+                $anioNac = isset($_GET['anioNac']) ? $_GET['anioNac'] : '';
+                $telefono = isset($_GET['telefono']) ? $_GET['telefono'] : '';
             echo '<tr>';
-                
+                echo  "<td>$nombre</td><td>$prApe "."$sgApe</td><td>$email</td><td>$anioNac</td><td>$telefono</td>";
 
             echo '</tr>';
             ?>
