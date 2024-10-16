4aa2ae3519c23c298f172f93bc9350d2dd36e71b
javixyloque <perosa1000@gmail.com>
2024-10-16 12:12:21 +0200

    16/10

--- 235palindromo.php
+++ 235palindromo.php
@@ -10,16 +10,37 @@
     <?php
     function palindroma ($palabra) {
         $palReves = "";
+        for ($i = 0; $i<strlen($palabra)-1;$i++) {
+            switch (ord($palabra)) {
+                case "á":
+
+            }
+            // if (ord($palabra[$i]) == ord('é')||ord($palabra[$i]) == ord('á')|| ord($palabra[$i]) == ord('í')||ord($palabra[$i]) == ord('ú')|| ord($palabra[$i]) == ord('ó')) {
+                
+            // }
+        }
+
         for ($i = strlen($palabra)-1; $i >= 0 ; $i--) {
             $palReves.=$palabra[$i];
         }
+
         echo $palabra." al revés es: ".$palReves."<br>";
+        
         if ($palabra == $palReves) {
-            echo $palabra." se lee igual de adelante hacia atrás y viceversa, es palindroma<br>";
+            return true;
+        } else {
+            return false;
         }
     }
-    palindroma("owo");
+    $palabra = "owó";
+    $ejemplo = palindroma($palabra);
 
+    if ($ejemplo) {
+        echo $palabra." se lee igual de adelante hacia atrás y viceversa, es palindroma<br>";
+    } else {
+        echo "$palabra no es palindroma porque al reves no es igual";
+    }
     ?>
+
 </body>
 </html>
