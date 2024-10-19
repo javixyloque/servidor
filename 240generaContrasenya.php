43fafec2e1d71fb3f1c20f36fbc1cd16245e6be7
javixyloque <perosa1000@gmail.com>
2024-10-19 19:09:23 +0200

    retoques 240generaContrasenya

--- 240generaContrasenya.php
+++ 240generaContrasenya.php
@@ -12,11 +12,18 @@
         // AL GENERARLO DE ESTA FORMA, EL MAXIMO TAMAñO DE CONTRASEñA
         // ES COMO MUCHO 32(EN HEXADECIMAL), YA QUE EL HASH ES DE 128 BITS.
         // SE PODRIA HACER MAS LARGO EN BINARIO, PERO PARA CONTRASEñAS MEJOR NO 
-        $tam = 6;
+        $tam = 54;
 
         function createPass ($size): string { 
-            $pass = md5(rand(PHP_INT_MIN, PHP_INT_MAX), false);
-            return substr($pass,0, $size);
+            $pass = "";
+
+            for ($i = 0; $i < $size; $i++)  {
+                // PARA QUE SEA MAS IMPREDECIBLE, CADA PASO DEL BUCLE GENERA UN MD5 DE UNA MINUSCULA
+                // Y OBTIENE UN CARACTER EN UN INDICE ALEATORIO DE ESOS 32 CARACTERES HEXADECIMALES
+                $temp= md5(chr(rand(97, 122)));
+                $pass.=$temp[rand(0, 31)];
+            }
+            return $pass;
         }
         echo"NUEVA CONTRASEÑA: <strong>". createPass ($tam)."</strong>";
 
