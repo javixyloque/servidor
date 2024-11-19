84c531f11cda008baf458f82eb16581605be61c5
javixyloque <perosa1000@gmail.com>
2024-11-19 14:03:32 +0100

    sadfigoiaeshjawergf

--- PDO/insertar_imagen.php
+++ PDO/insertar_imagen.php
@@ -2,6 +2,29 @@
     require_once('./conexionPDO.php');
     $conexion = conexion();
 
-    $sql = "INSERT INTO proyecto";
+    $titulo = isset($_POST['titulo'])?filtrado($_POST['titulo']): '';
+    $logotipo = isset($_FILES['logotipo'])? filtrado($_FILES['logotipo']['name']): '';
+
+    $sql = "INSERT INTO proyecto (titulo, logotipo) values (:titulo, :logotipo)";
+    try {
+        $insert = $conexion->prepare($sql);
+        $insert -> bindParam(':titulo', $titulo);
+        $insert -> bindParam(':logotipo', $logotipo);
+        $resultado = $insert -> execute();
+        if ($resultado) {
+            $ultimoId = $conexion->lastInsertId();
+            $logotipoTemp = $_FILES['logotipo']['tmp_name'];
+            $logotipoFin = './imagenes/'.$ultimoId.'-'.$logotipo;
+            move_uploaded_file($logotipoTemp, $logotipoFin);
+        }
+    } catch (PDOException $e) {
+        error_log("Error en la operacion".$e->getMessage());
+    }finally {
+
+        $conexion = null;
+    }
+    
+
+
 
 ?>
