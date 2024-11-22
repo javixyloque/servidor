4564d2f1b95c45dab19d56d3600dc4c6811b594a
javixyloque <perosa1000@gmail.com>
2024-11-22 14:08:37 +0100

    clase clinete

--- CRUD_gestion_proyecto/vista/form_editar.php
+++ CRUD_gestion_proyecto/vista/form_editar.php
@@ -1,28 +1,45 @@
 <?php
-    require_once('../conexion/conexionPDO.php');
+    require_once('../conexion/conexion.php');
     $conexion = conexion();
 
-    $id = isset($_POST['id_proyecto'])? filtrado($_POST['id_proyecto']): '';
-    $titulo = isset($_POST['titulo'])? filtrado($_POST['titulo']): '';
-    $descripcion = isset($_POST['descripcion'])? filtrado($_POST['descripcion']): '';
-    $periodo = isset($_POST['periodo'])? filtrado($_POST['periodo']): '';
-    $curso = isset($_POST['curso'])? filtrado($_POST['curso']): '';
-    $fecha = isset($_POST['fecha'])? filtrado($_POST['fecha']): '';
-    $nota = isset($_POST['nota'])? filtrado($_POST['nota']): '';
-    $logotipo = isset($_FILES['logotipo']) ? $_FILES['logotipo'] : '';
-    $pdf = isset($_FILES['pdf_proyecto']['name'])? $_FILES['pdf_proyecto']['name']: '';
-    
-    
-    try {
-        $sql = "SELECT * FROM proyecto WHERE id_proyecto = :id";
+    $id = isset($_GET['id'])? intval(filtrado($_GET['id'])): '';
+    // $titulo = isset($_POST['titulo'])? filtrado($_POST['titulo']): '';
+    // $descripcion = isset($_POST['descripcion'])? filtrado($_POST['descripcion']): '';
+    // $periodo = isset($_POST['periodo'])? filtrado($_POST['periodo']): '';
+    // $curso = isset($_POST['curso'])? filtrado($_POST['curso']): '';
+    // $fecha = isset($_POST['fecha'])? filtrado($_POST['fecha']): '';
+    // $nota = isset($_POST['nota'])? filtrado($_POST['nota']): '';
+    // $logotipo = isset($_FILES['logotipo']) ? $_FILES['logotipo'] : '';
+    // $pdf = isset($_FILES['pdf_proyecto']['name'])? $_FILES['pdf_proyecto']['name']: '';
+    // INICIALIZAR VARIABLES PARA EVITAR ERRORES
+    $titulo = $descripcion = $periodo = $curso = $fecha = $nota = $logotipo = '';
 
-        $sentencia = $conexion -> prepare($sql);
-        $sentencia -> setFetchMode(PDO::FETCH_ASSOC);
-        $sentencia -> execute();
-
-    } catch (PDOException $e) {
-        echo "Error: ".$e->getMessage();
-        }
+    if(!empty($id)) {
+        try {
+            $sql = "SELECT * FROM proyecto WHERE id_proyecto = :id";
+    
+            $sentencia = $conexion -> prepare($sql);
+            $sentencia -> bindParam(':id', $id, PDO::PARAM_INT);
+            $sentencia -> execute();
+            $select = $sentencia -> fetch(PDO::FETCH_ASSOC);
+    
+            if ($select) {
+                $titulo = $select['titulo'];
+                $descripcion = $select['descripcion'];
+                $periodo = $select['periodo'];
+                $curso = $select['curso'];
+                $fecha = $select['fecha_presentacion'];
+                $nota = $select['nota'];
+                $logotipo = $select['logotipo'];
+            } 
+            
+            
+    
+        } catch (PDOException $e) {
+            echo "Error: ".$e->getMessage();
+            }
+    }
+    
 
 
 ?>
@@ -36,42 +53,45 @@
     <title>Subir proyecto</title>
 </head>
 <body>
+    <h2>FORMULARIO DE MODIFICACIÓN DEL TRABAJO <?=  filtrado($titulo) ?></h2>
     <form action="../controlador/editar.php" method="post" enctype="multipart/form-data">
         
             <label for="titulo">Titulo: </label>
-                <input type="text" name="titulo" value="$">
+                <input type="text" name="titulo" value="<?= filtrado($titulo) ?>">
             
         
         
             <label for="descripcion">Descripción: </label>
-                <input type="text" name="descripcion">
+                <input type="text" name="descripcion" value="<?= filtrado($descripcion) ?>">
             
             
         
         
             <label for="periodo">Periodo: </label>
-                <input type="text" name="periodo">
+                <input type="text" name="periodo" value="<?= filtrado($periodo) ?>">
             
             
         
         
             <label for="curso">Curso: </label>
-                <input type="number">
+                <input type="number" value="<?= intval(filtrado($curso)) ?>">
             
             
         
         
             <label for="fecha">Fecha: </label>
-                <input type="text" name="fecha">
+                <input type="text" name="fecha" value="<?= filtrado($fecha) ?>">
             
             
         
         
             <label for="nota">Nota: </label>
-                <input type="text" name="nota">
+                <input type="text" name="nota" value="<?= intval(filtrado($nota)) ?>">
             
-            
-            <label for="logotipo">Logo: </label>
+            <label for="logotipo">Logo actual:</label>
+                <img src="data:image/jpeg;base64,<?= base64_encode($logotipo) ?>" alt="Logotipo actual" style="max-width: 150px; max-height: 150px;">
+    
+            <label for="logotipo">Nuevo logo: </label>
                 <input type="file" name="logotipo" enctype="multipart/form-data">
             
 
