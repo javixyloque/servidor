43fc48cbc91940348b67fdfc72ad998d936241d3
javixyloque <perosa1000@gmail.com>
2024-11-25 22:39:58 +0100

    Me cansé

--- FPDF/fwrite.php
+++ FPDF/fwrite.php
@@ -1,8 +1,7 @@
 <?php
     // CREAMOS, ESCRIBIMOS Y CERRAMOS UN FICHERO
-
     $archivo = "miarchivo.txt";
-    $texto = "Hola, esto es un ejemplo de archivo de texto.\n";
+    $texto = "FUNCIONA POR FAVOR";
 
     // SI QUEREMOS AÑADIR TEXTO, DEBE ABRIRSE A+
     $abrir = fopen($archivo, "w");
