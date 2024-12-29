7d8f8e2f6204bbaf5f76dc33f89df8bc6592346a
javixyloque <perosa1000@gmail.com>
2024-12-29 16:29:02 +0100

    controlador

--- PRACTICA_GESTION_ALUMNOS/controlador/index.js
+++ PRACTICA_GESTION_ALUMNOS/controlador/index.js
@@ -7,26 +7,47 @@ document.addEventListener("DOMContentLoaded", () => {
     const iniciarSesion = document.querySelector('.iniciar_sesion'); // Formulario iniciar sesión
     const registrarse = document.querySelector('.registrarse'); // Formulario registrarse
 
+    const formInicio = document.getElementById('iniciar_sesion');
+    const formRegistro = document.getElementById('registrarse');
+
+    iniciarSesion.style.backgroundColor = "#575F51";
+
     // Funciones para activar/desactivar triggers
-    function activarTrigger(trigger) {
+    function activarTrigger(trigger) { 
         trigger.setAttribute("data-status", "activo");
         trigger.style.backgroundColor = '#575F51';
-        
+        // AÑADIR FORMULARIO
+        if (trigger.classList.contains("iniciar_sesion")) {
+            formInicio.style.display = 'flex';
+            formInicio.style.flexDirection = 'column';
+        } else {
+            formRegistro.style.display = 'flex';            
+            formRegistro.style.flexDirection = 'column';
+        }
+
     }
 
     function desactivarTrigger(trigger) {
         trigger.setAttribute("data-status", "inactivo");
         trigger.style.backgroundColor = '#DAE3D1';
+        // ELIMINAR DISPLAY FORMULARIO
+        if (trigger.classList.contains("iniciar_sesion")) {
+            formInicio.style.display = 'none';
+        } else {
+            formRegistro.style.display = 'none';
+        }
     }
 
-    // Iterar sobre los triggers para agregar eventos
+    // BUCLE PARA AÑADIR LOS EVENTOS DE CLICK
     triggers.forEach((elemento) => {
         elemento.addEventListener("click", () => {
+
             if (elemento.classList.contains("iniciar_sesion")) {
-                activarTrigger(iniciarSesion);
+                activarTrigger(elemento);
                 desactivarTrigger(registrarse);
+
             } else if (elemento.classList.contains("registrarse")) {
-                activarTrigger(registrarse);
+                activarTrigger(elemento);
                 desactivarTrigger(iniciarSesion);
             }
         });
