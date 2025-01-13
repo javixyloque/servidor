19a62b3455dd9db5da70117218604df772bf20a4
javixyloque <perosa1000@gmail.com>
2025-01-13 10:06:22 +0100

    506_508

---
+++ CLASES/EJERCICIOS_506_508/506/506Persona.php
@@ -0,0 +1,88 @@
+<?php
+    declare(strict_types=1);
+    require_once'./Persona.php';
+
+
+    class Empleado extends Persona {
+        private float $salario;
+        private array $telefono;
+        private static int $sueldoTope = 1500;
+
+        public function __construct(string $nombre, string $apellidos, float $salario = 1000, $num=null) {
+            parent::__construct($nombre, $apellidos);
+            $this -> salario = $salario;
+            if ($num) {
+                $this -> anadirTelefono($num);
+            }
+        }
+
+
+
+        public function setNombre(string $nom): void {
+            $this -> nombre = $nom;
+        }
+
+        public function setSalario(float $sal): void {
+            $this -> salario = $sal;
+        }
+        public static function setSueldoTope(int $sueldo): void {
+            self::$sueldoTope = $sueldo;
+        }
+
+        
+        
+        public function getSalario(): float {
+            return $this->salario;
+        }
+        public static function getSueldoTope(): int {
+            return self::$sueldoTope;
+        }
+
+
+        
+        public function listarTelefonos(): string {
+            $completo = '';
+            foreach ($this -> telefono as $tel) {
+                $completo.= $tel.", ";
+            }
+            $final = substr($completo, 0, strlen($completo)-2);
+            return $final;
+        }
+
+        public function vaciarTelefonos(): void {
+            $this -> telefono = [];
+        }
+
+        public function anadirTelefono(int $tel): void {
+            array_push(($this -> telefono), $tel);
+        }
+
+
+        public function debePagarImpuestos(): bool {
+            
+            if ($this->salario <= self::$sueldoTope)
+            return false;
+            else
+            return true;
+            
+        }
+
+        public function getDatosCompleto(): string {
+            if (empty($this->telefono)) {
+                return parent::getDatosCompleto()."<br>".$this->getSalario()." €";
+            }
+            return "EMPLEADO: ".$this->getNombre()."<br>Teléfonos: ".$this->listarTelefonos()."<br>".$this->getSalario()." €";
+        }
+    }
+
+    $obj1 = new Empleado("Juan", "Gonzalez", 2000);
+    if ($obj1 -> debePagarImpuestos()) {
+        echo $obj1 -> getDatosCompleto(). ".<br><br> Además, tiene que pagar impuestos";
+    } else {
+        echo $obj1 -> getDatosCompleto(). ".<br><br> No tiene que pagar impuestos";
+    }
+    
+    // echo $obj1 ->getDatosCompleto();
+
+?>
+
---
+++ CLASES/EJERCICIOS_506_508/506/Persona.php
@@ -0,0 +1,60 @@
+<?php
+    declare(strict_types=1);
+
+class Persona {
+    protected string $nombre;
+    protected string $apellidos;
+    protected int $edad;
+
+
+    
+    public function __construct(string $nombre, string $apellidos) {
+        $this -> nombre = $nombre;
+        $this -> apellidos = $apellidos;
+    }
+
+
+    public function setNombre(string $nom): void {
+        $this -> nombre = $nom;
+    }
+
+    // public function setEdad(int $edad): void {
+    //     $this -> edad = $edad;
+    // }
+    // public function getEdad(): int {
+    //     return $this->edad;
+    // }
+    
+
+    public function getNombre(): string {
+        return $this->nombre;
+    }
+    public function getApellidos(): string {
+        return $this->apellidos;
+    }
+    public function setApellidos(string $ape): void {
+        $this -> apellidos = $ape;
+    }
+    
+    
+
+    public function getDatosCompleto(): string {
+        return "EMPLEADO: ".$this->getNombre()." ".$this->getApellidos();
+    }
+
+}
+
+
+
+
+// $insert = $conexion->prepare("INSERT INTO persona(nombre, telefono) VALUE (?, ?) ");
+//     $insert -> bind_param("si", $nombre, $telefono);
+
+//     if ($insert->execute()) {
+//         echo "Registro insertado correctamente<br>";
+//     } else {
+        
+//     }
+//     $conexion->close();
+
+// ?>
