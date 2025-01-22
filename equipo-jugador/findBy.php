a7ad0a1a41738dbe5fc0372543deeb25df77961e
javixyloque <perosa1000@gmail.com>
2025-01-22 14:07:01 +0100

    CLASEW

--- equipo-jugador/findBy.php
+++ equipo-jugador/findBy.php
@@ -1,12 +1,30 @@
 <?php
 require_once "bootstrap.php";
-require_once './src/Jugador.php';
-require_once './src/Equipo.php';
+require_once './src/Entity/Jugador.php';
+require_once './src/Entity/Equipo.php';
 
 /*Con findBy/findOneBy:
 - Jugadores con exactamente XX años..*/
 echo "Jugadores con 12 años<br>";
 $jugadores = $entityManager->getRepository('Jugador')->findBy(array('edad' => 12));
+
+echo "<br>"; 
+
+
+// Jugadores mayores de 12 años
+// NO SE PUEDE CON findBy
+
+// echo "Jugadores mayores de 12 años<br>";
+// $jugadores = $entityManager->createQueryBuilder()
+//     ->select('j')
+//     ->from('Jugador', 'j')
+// 	->join('Equipo', 'e')
+//     ->where('j.edad > :edad')
+//     ->setParameter('edad', 12)
+//     ->getQuery()
+//     ->getResult();
+
+
 
 foreach($jugadores as $jugador)
 {
@@ -14,7 +32,7 @@ foreach($jugadores as $jugador)
 
 }
 
-//Equipos de Madrid fundados en 1900.
+// Equipos de Madrid fundados en 1900.
 echo "Equipos de Madrid fundados en 1900<br>";
 $equipos = $entityManager->getRepository('Equipo')->findBy(array('fundacion' => 1900, 'ciudad'=>'Madrid'));
 foreach($equipos as $equipo)
