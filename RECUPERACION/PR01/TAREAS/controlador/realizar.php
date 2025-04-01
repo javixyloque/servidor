d839b624cec889e85ae09f52be49b0e7ecf75568
javixyloque <perosa1000@gmail.com>
2025-04-02 00:56:02 +0200

    Se acabó lo que sedaba

--- RECUPERACION/PR01/TAREAS/controlador/realizar.php
+++ RECUPERACION/PR01/TAREAS/controlador/realizar.php
@@ -2,9 +2,19 @@
 require_once'./controlador.php';
 
 $id = filtrado($_GET['id'])?? '';
-
+$archivo = "../vista/Javier/hechasJavier.txt";
 try {
     realizarTarea($id);
+    $tareaRealizada = selectTareaId($id);
+
+    if (file_exists($archivo)) {
+        $nuevaFila = " $tareaRealizada[titulo]: $tareaRealizada[descripcion]. Fecha:". substr($tareaRealizada['fecha'],8,2) ." / ". substr($tareaRealizada['fecha'],5,2)." / ".   substr($tareaRealizada['fecha'],0,4)." - Prioridad: $tareaRealizada[prioridad]" ;
+        file_put_contents($archivo, $nuevaFila. "\n", FILE_APPEND);
+
+    } else  {
+        $contenido_archivo = "TAREAS REALIZADAS\n\n $tareaRealizada[titulo]: $tareaRealizada[descripcion]. Fecha:". substr($tareaRealizada['fecha'],8,2) ." / ". substr($tareaRealizada['fecha'],5,2)." / ".   substr($tareaRealizada['fecha'],0,4)." - Prioridad: $tareaRealizada[prioridad]" ;
+        file_put_contents($archivo, $contenido_archivo. "\n");
+    }
     sleep(3);
     echo "<h2>Tarea realizada correctamente.</h2><br><h3>Volviendo a las tareas ...</h3>";
     header("Location: ../vista/tareas.php");
