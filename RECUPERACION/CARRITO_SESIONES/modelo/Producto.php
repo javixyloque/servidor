10d7bb2dd779e6c09163a4139fa56ffb70b15821
javixyloque <javixyloque@gmail.com>
2025-04-22 00:30:27 +0200

    A la puta cama ya hombre

---
+++ RECUPERACION/CARRITO_SESIONES/modelo/Producto.php
@@ -0,0 +1,75 @@
+<?php
+
+
+class Producto {
+    private String $nombre;
+    private float $precio;
+    private int $cantidad;
+
+    
+    public function __construct ($nom, $pre) {
+        $this->nombre = $nom;
+        $this->precio = $pre;
+        
+    }
+
+
+    /**
+     * Get the value of nombre
+     */
+    public function getNombre(): String
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set the value of nombre
+     */
+    public function setNombre(String $nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of precio
+     */
+    public function getPrecio(): float
+    {
+        return $this->precio;
+    }
+
+    /**
+     * Set the value of precio
+     */
+    public function setPrecio(float $precio): self
+    {
+        $this->precio = $precio;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of cantidad
+     */
+    public function getCantidad(): int
+    {
+        return $this->cantidad;
+    }
+
+    /**
+     * Set the value of cantidad
+     */
+    public function setCantidad(int $cantidad): self
+    {
+        $this->cantidad = $cantidad;
+
+        return $this;
+    }
+
+    public function sumarProducto(): self {
+        $this->cantidad++;
+        return $this;
+    }
+}
